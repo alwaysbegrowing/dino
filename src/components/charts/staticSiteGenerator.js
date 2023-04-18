3d98469e1d0689529112338affb30fd56abf339e
Bookcliff <bookcliff76@gmail.com>
2023-04-18 08:46:56 -0500

    add ssg

---
+++ src/components/charts/staticSiteGenerator.js
@@ -0,0 +1,35 @@
+import { Pie } from "@ant-design/plots";
+import { useEffect, useState } from "react";
+import { getCleanedData } from "./cdnPie";
+
+const SsgPie = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    getCleanedData("static-site-generator", setData);
+  }, []);
+
+  const ssgConfig = {
+    appendPadding: 10,
+    data,
+    angleField: "value",
+    colorField: "name",
+    radius: 0.9,
+    label: {
+      type: "inner",
+      offset: "-30%",
+      content: ({ percent }) => `${(percent * 100).toFixed(0)}%`,
+      style: {
+        fontSize: 14,
+        textAlign: "center",
+      },
+    },
+    interactions: [
+      {
+        type: "element-active",
+      },
+    ],
+  };
+  return <Pie {...ssgConfig} />;
+};
+
+export default SsgPie;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import { Row, Col, Button } from "antd";
 import CDNPie from "../components/charts/cdnPie";
 import PaasPie from "../components/charts/paasPie";
+import SsgPie from "../components/charts/staticSiteGenerator";
 
 const GraphsPage = () => {
   return (
     <>
       <CDNPie />
       <PaasPie />
+      <SsgPie />
     </>
   );
 };
