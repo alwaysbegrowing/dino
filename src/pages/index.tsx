63738fd9d1dd26da71e4f06eb592f5fea170def3
Bookcliff <bookcliff76@gmail.com>
2023-04-18 08:45:16 -0500

    add paas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,14 @@
 import { Row, Col, Button } from "antd";
 import CDNPie from "../components/charts/cdnPie";
+import PaasPie from "../components/charts/paasPie";
 
 const GraphsPage = () => {
-  return <CDNPie />;
+  return (
+    <>
+      <CDNPie />
+      <PaasPie />
+    </>
+  );
 };
 
 export default GraphsPage;
