d5a1919673fec5d580bea6b1075845158be2d85e
Bookcliff <bookcliff76@gmail.com>
2023-04-18 08:52:16 -0500

    add pagebuilders

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,23 @@ import { Row, Col, Button } from "antd";
 import CDNPie from "../components/charts/cdnPie";
 import PaasPie from "../components/charts/paasPie";
 import SsgPie from "../components/charts/staticSiteGenerator";
-
+import PageBuildersPie from "../components/charts/pageBuilders";
+import { Typography } from "antd";
+const { Title } = Typography;
 const GraphsPage = () => {
   return (
     <>
+      <Title style={{ color: "white" }}>CDNs</Title>
       <CDNPie />
+      <Title style={{ color: "white" }}>PAAS</Title>
+
       <PaasPie />
+      <Title style={{ color: "white" }}>Static Site Generator</Title>
+
       <SsgPie />
+      <Title style={{ color: "white" }}>Page Builders</Title>
+
+      <PageBuildersPie />
     </>
   );
 };
